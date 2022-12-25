2667719ef7dde46d44c97095febca6654d3046e4
gthomas513 <gabe.thomas513@gmail.com>
2022-12-25 18:15:00 -0500

    changed celsius to fahrenheit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   const search = evt => {
     if (evt.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
         .then(res => res.json())
         .then(result => {
           setWeather(result);
@@ -54,7 +54,7 @@ function App() {
           </div>
           <div className="weather-box">
             <div className="temp">
-              {Math.round(weather.main.temp)}°c
+              {Math.round(weather.main.temp)}°F
             </div>
             <div className="weather">{weather.weather[0].main}</div>
           </div>
